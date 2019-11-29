072524d8d248593e46e16cc0b5dc951f4536720f
cgardn <christopher.gardn@gmail.com>
2019-11-29 13:41:41 -0500

    exercises 4 and 5

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,27 @@
-#write your code here
+def translateWord word
+    v = ['a','e','i','o','u']
+    out = ''
+
+    if word.index('qu')
+        i = word.index('qu')+2
+        out = word[i,word.length] + word[0,i] + 'ay'
+    elsif
+        word.each_char do |c|
+            if v.include? c
+                i = word.index(c)
+                out = word[i,word.length] + word[0,i] + 'ay'
+                break
+            end 
+        end
+    end
+
+    return out
+end
+
+def translate words
+    arr = words.split
+    arr.each_index do |i|
+        arr[i] = translateWord(arr[i])
+    end
+    return arr.join(' ').strip
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+
+    attr_reader :title
+
+    def title=(t)
+        @title = titleFormat(t)
+    end
+
+    def titleFormat(t)
+        no_list = ['a','an','the','and','in','of','over','at']
+        t = t.split
+        t[0] = t[0].capitalize
+        out = t[1,t.length].map do |x|
+            if (!no_list.include?(x))
+                x.capitalize
+            else
+                x.downcase
+            end
+        end
+        out.unshift(t[0])
+        return out.join(' ')
+    end
 end
